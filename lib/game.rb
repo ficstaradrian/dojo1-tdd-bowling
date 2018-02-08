2430a30ed7e41f59050aab78d7e327f8a0e3cbde
Adrian Diaz <web.art@adriandiaz.ca>
2018-02-08 00:06:24 -0500

    Adds bonus score for spare

--- lib/game.rb
+++ lib/game.rb
@@ -3,10 +3,11 @@ class Game
       previous_frame = nil
       frame_history.map do |frame|
         if previous_frame && previous_frame == 10
-          frame
+          frame = Array.new << frame[0] * 2 << frame[1]
+          puts frame.inspect
         end
         previous_frame = frame.reduce(:+)
         frame.reduce(:+)
-      end.first
+      end.flatten.reduce(:+)
     end
 end
